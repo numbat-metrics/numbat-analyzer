1ab6ab7f1e72bad5f39c55110ba65991f23c9df8
Maciej Małecki <me@mmalecki.com>
2015-07-15 18:20:12 +0200

    Add the `LoadAverage` rule to the example

--- examples/ruleset.js
+++ examples/ruleset.js
@@ -1,11 +1,24 @@
 var
     EchoRule  = require('../lib/rules/echo.js'),
-    Heartbeat = require('../lib/rules/heartbeat.js')
+    Heartbeat = require('../lib/rules/heartbeat.js'),
+    LoadAverage = require('../lib/rules/load-average.js')
     ;
 
 var rules = [];
 
 rules.push(new EchoRule());
 rules.push(new Heartbeat());
+rules.push(new LoadAverage({
+    warn: {
+        1: 1.5,
+        5: 0.9,
+        15: 0.7
+    },
+    critical: {
+        1: 1.5,
+        5: 1.5,
+        15: 1.5
+    }
+}));
 
 module.exports = rules;
