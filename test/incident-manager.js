36fe9c8be0bee4f43ad16e9e63ac8e6f8a57e7f6
Maciej Małecki <me@mmalecki.com>
2015-08-23 12:53:14 +0200

    Add a incident manager

---
+++ test/incident-manager.js
@@ -0,0 +1,46 @@
+var
+    assert = require('assert'),
+    IncidentManager = require('../lib/incident-manager')
+    ;
+
+var incidentManager = new IncidentManager({ ttl: 100 });
+var gotIncident = false;
+var gotExpiry = false;
+
+incidentManager.on('readable', function()
+{
+    var incident = incidentManager.read();
+    assert.equal(incident.host, 'test');
+    assert.equal(incident.name, 'heartbeat');
+    assert(incident.id);
+
+    if (incident.status === 'critical')
+    {
+        assert(!gotIncident, 'incident should not have expired');
+        gotIncident = true;
+    }
+    else
+    {
+        assert.equal(incident.status, 'ok', 'incident should have expired');
+        gotExpiry = true;
+    }
+});
+
+function ping()
+{
+    incidentManager.write(
+    {
+        host: 'test',
+        name: 'heartbeat',
+        status: 'critical'
+    });
+}
+
+ping();
+setTimeout(ping, 50);
+
+process.on('exit', function()
+{
+    assert(gotIncident);
+    assert(gotExpiry);
+});
